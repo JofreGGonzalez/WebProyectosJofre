713f1d78cbb7d3986cb43325f2bed757795bcaa5
JofreGGonzalez <jofreggonzalez@gmail.com>
2025-06-22 18:08:28 +0200

    Resolución de errores durante el testeo. Cambio de videos a Loop. Cambios de GIF a videos. Añadir mas info a la portada.

--- js/galeria.js
+++ js/galeria.js
@@ -1,7 +1,7 @@
 function showImg(){
     // PARA GALERIA
     // (A) GET LIGHTBOX & ALL .ZOOMD IMAGES
-    let all = document.getElementsByClassName("galeriaTrueImg"),
+    let all = document.querySelectorAll(".galeriaTrueImg, .galeriaTrueVideo"),
         lightbox = document.getElementById("lightbox");
 
     // (B) CLICK TO SHOW IMAGE IN LIGHTBOX
@@ -9,8 +9,20 @@ function showImg(){
     if (all.length>0) {
         for (let i of all) {
             i.onclick = () => {
-                let clone = i.cloneNode();
-                clone.className = "";
+                let clone;
+                if (i.tagName.toLowerCase() === 'video') {
+                    clone = i.cloneNode(true);
+                    clone.className = "";
+                    clone.setAttribute("controls", "controls");
+                    clone.setAttribute("autoplay", "autoplay");
+                } else {
+                    clone = i.cloneNode();
+                    clone.className = "";
+                    if (i.classList.contains("pixelArt")) {
+                        clone.classList.add("pixelated");
+                    }
+                }
+
                 lightbox.innerHTML = "";
                 lightbox.appendChild(clone);
                 lightbox.className = "show";
@@ -20,29 +32,7 @@ function showImg(){
     // (C) CLICK TO CLOSE LIGHTBOX
     lightbox.onclick = () => {
         lightbox.className = "";
-    };
-
-    //PARA QHE HACEMOS
-    // (A) GET LIGHTBOX & ALL .ZOOMD IMAGES
-    let all2 = document.getElementsByClassName("circleTrueImg"),
-        lightbox2 = document.getElementById("lightbox2");
-
-    // (B) CLICK TO SHOW IMAGE IN LIGHTBOX
-    // * SIMPLY CLONE INTO LIGHTBOX & SHOW
-    if (all2.length>0) {
-        for (let i of all2) {
-            i.onclick = () => {
-                let clone = i.cloneNode();
-                clone.className = "";
-                lightbox2.innerHTML = "";
-                lightbox2.appendChild(clone);
-                lightbox2.className = "show";
-            };
-        }}
-
-    // (C) CLICK TO CLOSE LIGHTBOX
-    lightbox2.onclick = () => {
-        lightbox2.className = "";
+        lightbox.innerHTML = "";
     };
 }
 
